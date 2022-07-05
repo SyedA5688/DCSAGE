73849f3bd4818944249272efac2419f52a0480be
Syed Rizvi <syedrizvi@Syeds-MBP.lan>
2022-07-05 09:02:57 -0500

    fixed comments and added docstrings to test and recursive prediction scripts

--- training/recursive_prediction.py
+++ training/recursive_prediction.py
@@ -15,6 +15,19 @@ from models.dcsage_temporal_attn import DCSAGE_Temporal_Attn
 
 
 def recursive_test(saved_preds_dir, args, test_dataloader, test_dataset, model_path, save_path):
+    """
+    This function runs recursive prediction evaluation on the test dataset using the best
+    model from training. This function should be run after regular test evaluation.
+
+    Arguments:
+    - saved_preds_dir: path to directory containing saved predictions on test set
+    - args: arguments dictionary
+    - test_dataloader: the test dataloader
+    - test dataset: test dataset, needed separately for accessing some arrays
+    - model_path: path to best trained model from training
+    - save_path: path to directory where recursive prediction trends should be saved
+    """
+    # ============ Define model ============
     if args["model_architecture"] == "DCSAGE":
         model = DynamicAdjSAGE(node_features=args['num_node_features'], emb_dim=args['embedding_dim'], window_size=args["window"], output=1, training=True, lstm_type=args["lstm_type"], name="DASAGE")
     elif args["model_architecture"] == "DCSAGE_GRU":
@@ -30,6 +43,7 @@ def recursive_test(saved_preds_dir, args, test_dataloader, test_dataset, model_p
     else:
         raise NotImplementedError("Model architecture not implemeted.")
 
+    # ============ Load test set predictions saved earlier by test function ============
     with open(os.path.join(saved_preds_dir, "test_predictions_save.json"), "r") as f:
         saved_dict = json.load(f)
     test_preds, test_labels = saved_dict['test_predictions'], saved_dict['test_labels']
@@ -37,38 +51,40 @@ def recursive_test(saved_preds_dir, args, test_dataloader, test_dataset, model_p
     test_preds_np = np.array([np.array(nested_list) for nested_list in test_preds])
     test_labels_np = np.array([np.array(nested_list) for nested_list in test_labels])
 
-
+    # ============ Load best checkpoint from training ============
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
 
     with torch.no_grad():
+        # ============ Recursive test loop ============
         extended_window_ncases = torch.zeros(len(test_dataset) + args["window"], 10)
-        extended_window_ncases[0:args["window"],:] = torch.from_numpy(test_dataset[0][0][:,:,0])  # Give first 7 real days
+        # Give first 7 days of data to model to start recursive prediction
+        extended_window_ncases[0:args["window"],:] = torch.from_numpy(test_dataset[0][0][:,:,0])
 
         for batch_window_node_feat, batch_window_edge_idx, batch_window_edge_attr, batch_window_labels in test_dataloader:
 
             for window_idx in range(len(batch_window_node_feat)):
-                window_node_feat = batch_window_node_feat[window_idx]  # shape [14, 10, 3]
-                window_edge_idx = batch_window_edge_idx[window_idx]  # shape [14, 2, 100]
-                window_edge_attr = batch_window_edge_attr[window_idx]  # shape [14, 100, 1]
-                window_labels = batch_window_labels[window_idx]  # shape [10]
+                window_node_feat = batch_window_node_feat[window_idx]
+                window_edge_idx = batch_window_edge_idx[window_idx]
+                window_edge_attr = batch_window_edge_attr[window_idx]
+                window_labels = batch_window_labels[window_idx]
                 
-                # Overwrite num_cases node feature with model predictions starting at 2nd window
+                # Overwrite num_cases feature with model predictions
                 if window_idx > 0:
                     window_node_feat[:,:,0] = extended_window_ncases[window_idx:window_idx + args["window"]]
 
                 h_1, c_1, h_2, c_2 = None, None, None, None
                 for day_idx in range(len(window_node_feat)):
-                    day_node_feat = window_node_feat[day_idx]  # shape [10, 3]
-                    day_edge_idx = window_edge_idx[day_idx]  # shape [2, 100]
-                    day_edge_attr = window_edge_attr[day_idx]  # shape [100, 1]
+                    day_node_feat = window_node_feat[day_idx]
+                    day_edge_idx = window_edge_idx[day_idx]
+                    day_edge_attr = window_edge_attr[day_idx]
 
                     cutoff_idx = day_edge_idx[0].tolist().index(-1)
                     day_edge_idx = day_edge_idx[:, :cutoff_idx]
                     day_edge_attr = day_edge_attr[:cutoff_idx, :]
                     
-                    # Construct graph object for a single day inside of the sliding window, pass through model
+                    # Forward through model
                     day_graph = Data(x=day_node_feat, edge_index=day_edge_idx, edge_attr=day_edge_attr)
                     if args["model_architecture"] in ["DCSAGE", "DCGCN", "DCGIN"]:
                         y_hat, h_1, c_1, h_2, c_2 = model(day_graph, h_1, c_1, h_2, c_2, day_idx)
@@ -81,12 +97,13 @@ def recursive_test(saved_preds_dir, args, test_dataloader, test_dataset, model_p
                     else:
                         y_hat = model(day_graph, day_idx, target_seq=window_labels)
                     
-                # Append predictions to end of extended_window_num_cases
+                # Add model predictions to predictions array
                 extended_window_ncases[window_idx + args["window"], :] = y_hat[:,0]
         
-        # Remove first 14 days that were given from data. Matches length with labels and real predictions on test set, good for plotting
+        # Remove first 7 days that were given from data, keep only recursive predictions
         extended_window_ncases = extended_window_ncases[args["window"]:,:]
 
+        # ============ Plot recursive prediction trend ============
         if not os.path.exists(save_path):
             os.mkdir(save_path)
         plot_continent_pred_vs_ground_truth_vs_extended_feedback_pred_trend(test_preds_np, test_labels_np, extended_window_ncases, SAVE_PATH=save_path)
--- training/test.py
+++ training/test.py
@@ -16,6 +16,21 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def test(model_path, args, data_loader, SAVE_PATH, split_name):
+    """
+    This function runs evaluation on the test set with the best trained model from training.
+    Several things will be saved by this function: model predictions on the test set, a colored
+    scatter plot of predictions vs labels, any attention heatmaps applicable to the model,
+    test set prediction trends, and test set prediction Cumulative Distribution plots.
+
+    Arguments:
+    - model_path: path to best trained model checkpoint from training
+    - args: arguments dictionary from training
+    - data_loader: data loader for which evaluation should be run on (doesn't necessarily have
+                    be the test dataloader)
+    - SAVE_PATH: directory where test results should be saved (expected to already exist)
+    - split_name: name of dataset split which test is being run on (e.g. test, val, train)
+    """
+    # ============ Define model ============
     if args["model_architecture"] == "DCSAGE":
         model = DynamicAdjSAGE(node_features=args['num_node_features'], emb_dim=args['embedding_dim'], window_size=args["window"], output=1, training=True, lstm_type=args["lstm_type"], name="DASAGE")
     elif args["model_architecture"] == "DCSAGE_GRU":
@@ -36,9 +51,7 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
     model.eval()
 
     with torch.no_grad():        
-        ##########################
-        # Evaluate on test dataset
-        ##########################
+        # ============ Test loop ============
         predictions = []
         labels = []
         window_alphas_attn_coeff1 = []
@@ -47,27 +60,27 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
         for batch_window_node_feat, batch_window_edge_idx, batch_window_edge_attr, batch_window_labels in data_loader:
             
             for window_idx in range(len(batch_window_node_feat)):
-                window_node_feat = batch_window_node_feat[window_idx]  # shape [14, 10, 3]
-                window_edge_idx = batch_window_edge_idx[window_idx]  # shape [14, 2, 100]
-                window_edge_attr = batch_window_edge_attr[window_idx]  # shape [14, 100, 1]
-                window_labels = batch_window_labels[window_idx]  # shape [10]
+                window_node_feat = batch_window_node_feat[window_idx]
+                window_edge_idx = batch_window_edge_idx[window_idx]
+                window_edge_attr = batch_window_edge_attr[window_idx]
+                window_labels = batch_window_labels[window_idx]
 
                 day_alphas_attn_coeff1 = []
                 day_alphas_attn_coeff2 = []
 
                 h_1, c_1, h_2, c_2 = None, None, None, None
                 for day_idx in range(len(window_node_feat)):
-                    day_node_feat = window_node_feat[day_idx]  # shape [10, 3]
-                    day_edge_idx = window_edge_idx[day_idx]  # shape [2, 100]
-                    day_edge_attr = window_edge_attr[day_idx]  # shape [100, 1]
+                    day_node_feat = window_node_feat[day_idx]
+                    day_edge_idx = window_edge_idx[day_idx]
+                    day_edge_attr = window_edge_attr[day_idx]
 
                     cutoff_idx = day_edge_idx[0].tolist().index(-1)
                     day_edge_idx = day_edge_idx[:, :cutoff_idx]
                     day_edge_attr = day_edge_attr[:cutoff_idx, :]
                     
-                    # Construct graph object for a single day inside of the sliding window, pass through model
                     day_graph = Data(x=day_node_feat, edge_index=day_edge_idx, edge_attr=day_edge_attr)
 
+                    # Forward 1-day graph through model
                     if args["model_architecture"] in ["DCSAGE", "DCGCN", "DCGIN"]:
                         y_hat, h_1, c_1, h_2, c_2 = model(day_graph, h_1, c_1, h_2, c_2, day_idx)
                     elif args["model_architecture"] == "DCSAGE_GRU":
@@ -76,6 +89,7 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
                         y_hat, h_1, c_1, h_2, c_2, alphas_info = model(day_graph, h_1, c_1, h_2, c_2, day_idx)
                         (alphas_edge_index1, attention_weights1, alphas_edge_index2, attention_weights2) = alphas_info
 
+                        # Compute attention matrix
                         matr = np.zeros((10, 10))
                         matr2 = np.zeros((10, 10))
                         for i in range(alphas_edge_index1.shape[1]):
@@ -100,17 +114,18 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
                 elif args["model_architecture"] == "DCSAGE_Temporal_Attn":
                     window_alphas_attn_coeff1.append(np.array(h1_alphas))
                     window_alphas_attn_coeff2.append(np.array(h2_alphas))
-                # Save prediction and label on each sliding window for evaluation later
+                
+                # Save prediction and labels
                 predictions.append(y_hat)
                 labels.append(window_labels)
         
+        # ============ Save attention heatmaps where applicable ============
         if args["model_architecture"] == "DCGAT":
             window_alphas_attn_coeff1 = np.array(window_alphas_attn_coeff1)
             np.save(os.path.join(SAVE_PATH, "attn_coeff_1_matr.npy"), window_alphas_attn_coeff1)
             window_alphas_attn_coeff2 = np.array(window_alphas_attn_coeff2)
             np.save(os.path.join(SAVE_PATH, "attn_coeff_2_matr.npy"), window_alphas_attn_coeff2)
 
-            # Save heatmap
             plot_spatial_attn_coeff_heatmap(window_alphas_attn_coeff1.mean(axis=0).mean(axis=0), title="GAT Layer 1 Mean Spatial Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "gat_layer1_mean_attn_heatmap.png"))
             plot_spatial_attn_coeff_heatmap(window_alphas_attn_coeff1.sum(axis=0).sum(axis=0), title="GAT Layer 1 Summed Spatial Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "gat_layer1_summed_attn_heatmap.png"))
             plot_spatial_attn_coeff_heatmap(window_alphas_attn_coeff2.mean(axis=0).mean(axis=0), title="GAT Layer 2 Mean Spatial Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "gat_layer2_mean_attn_heatmap.png"))
@@ -122,14 +137,10 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
             np.save(os.path.join(SAVE_PATH, "temporal_attn_coeff_2_matr.npy"), window_alphas_attn_coeff2)
 
             # Save heatmap
-            # plot_temporal_attn_coeff_heatmap(window_alphas_attn_coeff1.mean(axis=0), seq_len=args["window"], title="LSTM Cell 1 Mean Temporal Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "lstm_cell1_mean_attn_heatmap.png"))
-            # plot_temporal_attn_coeff_heatmap(window_alphas_attn_coeff1.sum(axis=0), seq_len=args["window"], title="LSTM Cell 1 Summed Temporal Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "lstm_cell1_summed_attn_heatmap.png"))
             plot_temporal_attn_coeff_heatmap(window_alphas_attn_coeff2.mean(axis=0), seq_len=args["window"], title="LSTM Cell 2 Mean Temporal Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "lstm_cell2_mean_attn_heatmap.png"))
             plot_temporal_attn_coeff_heatmap(window_alphas_attn_coeff2.sum(axis=0), seq_len=args["window"], title="LSTM Cell 2 Summed Temporal Attention Coefficient Heatmap", save_path=os.path.join(SAVE_PATH, "lstm_cell2_summed_attn_heatmap.png"))
         
-        ############################################################
-        # Save all predictions into json file, make correlation plot
-        ############################################################
+        # ============ Savetest set predictions, make colored correlation plot ============
         save_colored_correlation_pred_vs_label_plot(predictions, 
             labels, 
             plot_title=split_name + ' Correlation Plot', 
@@ -138,7 +149,6 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
         plot_continent_pred_vs_ground_truth_trend(predictions, labels, split_name, SAVE_PATH=SAVE_PATH)
         plot_continent_CDFs(predictions, labels, split_name, SAVE_PATH=SAVE_PATH)
 
-        # Cast tensors to lists so that they are json serializable
         for i in range(len(predictions)):
             predictions[i] = [float(num) for num in predictions[i]]
             labels[i] = [float(num) for num in labels[i]]
@@ -149,9 +159,3 @@ def test(model_path, args, data_loader, SAVE_PATH, split_name):
         }
         with open(os.path.join(SAVE_PATH, "test_predictions_save.json"), "w") as test_f:
             json.dump(test_results, test_f, indent=4)
-
-"""
-matr = np.zeros((10, 10))
-for i in range(alphas_edge_index1.shape[1]):
-    matr[alphas_edge_index1[0,i], alphas_edge_index1[1,i]] = attention_weights1[i].mean()
-"""
